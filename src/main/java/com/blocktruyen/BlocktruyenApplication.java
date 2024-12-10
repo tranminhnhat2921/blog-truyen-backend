cada0cc9d05c845599cef484adc2f9a8142b8d07
TMN <tranminhnhat2921@gmail.com>
2024-12-10 21:54:24 +0700

    Init project

---
+++ src/main/java/com/blocktruyen/BlocktruyenApplication.java
@@ -0,0 +1,13 @@
+package com.blocktruyen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlocktruyenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlocktruyenApplication.class, args);
+	}
+
+}
